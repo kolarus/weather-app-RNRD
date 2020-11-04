b9f1e7746a2852eb4b8fed81a2ff6e1c66369ef6
Bohdan Morozov <Bohdan_Morozov@epam.com>
2020-11-04 21:17:52 +0200

    HW3: added const DEFAULT_ITEMS_QUANTITY

--- src/shared/utils/get-weather-by-time-range.ts
+++ src/shared/utils/get-weather-by-time-range.ts
@@ -1,5 +1,7 @@
 import {WeatherByTime} from 'src/shared/api/types';
 
+const DEFAULT_ITEMS_QUANTITY = 8;
+
 const getWeatherItemsByTimeRange = (
   visibleRange: {from: number; to: number},
   weatherItems: Array<WeatherByTime>,
@@ -7,15 +9,15 @@ const getWeatherItemsByTimeRange = (
   const weatherItemsInRange = weatherItems.filter((weatherItem) => {
     return weatherItem.dt >= visibleRange.from && weatherItem.dt <= visibleRange.to;
   });
-  if (weatherItemsInRange.length === 8) {
+  if (weatherItemsInRange.length === DEFAULT_ITEMS_QUANTITY) {
     return weatherItemsInRange;
   } else if (weatherItemsInRange.length === 0) {
-    return weatherItems.slice(0, 8);
+    return weatherItems.slice(0, DEFAULT_ITEMS_QUANTITY);
   } else {
     const indexOfLastItemInRange = weatherItems.findIndex((weatherItem) => {
       return weatherItemsInRange[weatherItemsInRange.length - 1].dt === weatherItem.dt;
     });
-    const numberOfItemsToAdd = 8 - weatherItemsInRange.length;
+    const numberOfItemsToAdd = DEFAULT_ITEMS_QUANTITY - weatherItemsInRange.length;
     const itemsToAdd = weatherItems.slice(
       indexOfLastItemInRange + 1,
       indexOfLastItemInRange + numberOfItemsToAdd,
